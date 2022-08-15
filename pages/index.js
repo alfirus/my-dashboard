a6567e2d4d55802e9c268af375e89ca3b97737aa
alfirus <alfirus@gmail.com>
2022-08-15 17:25:56 +0800

    Rebase new language

---
+++ pages/index.js
@@ -0,0 +1,58 @@
+export default function Home() {
+	return (
+		<>
+			<div class="row">
+				<div class="col-xl-3 col-md-6 col-sm-12">
+					<div class="card">
+						<div class="card-header">Business Info</div>
+						<div class="card-body">
+							<table class="table table-sm table-striped">
+								<tbody>
+									<tr>
+										<th>Company Name</th>
+										<td>ABC</td>
+									</tr>
+									<tr>
+										<th>Address</th>
+										<td>ABC</td>
+									</tr>
+									<tr>
+										<th>Email</th>
+										<td>ABC</td>
+									</tr>
+									<tr>
+										<th>Phone</th>
+										<td>ABC</td>
+									</tr>
+									<tr>
+										<th>Staff</th>
+										<td>123</td>
+									</tr>
+									<tr>
+										<th>Product</th>
+										<td>123</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-3 col-md-6 col-sm-12">
+					<div class="card">
+						<div class="card-body">Hello!</div>
+					</div>
+				</div>
+				<div class="col-xl-3 col-md-6 col-sm-12">
+					<div class="card">
+						<div class="card-body">Hello!</div>
+					</div>
+				</div>
+				<div class="col-xl-3 col-md-6 col-sm-12">
+					<div class="card">
+						<div class="card-body">Hello!</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
